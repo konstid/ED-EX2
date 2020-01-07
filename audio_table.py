f6e1436467fbcd7fe663bf00c05f5f9a97f59605
konstid <59444006+konstid@users.noreply.github.com>
2020-01-07 21:20:21 +0100

    Update audio_table.py

--- audio_table.py
+++ audio_table.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
+import arff
+
 
 #load average audio data and merge with ‚goodforairplane‘ attribute
 
 data_dev = pd.read_excel('data/CoE_dataset/Dev_Set/dev_set_groundtruth_and_trailers.xls').set_index('movie')
 data_test = pd.read_csv('data/CoE_dataset/Test_Set/test_set_inclGoodforairplane.csv').set_index('movie')
 
-indices = ['movie','MFCC 1', 'MFCC 2', 'MFCC 3', 'MFCC 4', 'MFCC 5',
-           'MFCC 6', 'MFCC 7', 'MFCC 8', 'MFCC 9', 'MFCC 10', 'MFCC 11', 'MFCC 12', 'MFCC 13']
+indices = ['movie','MFCC_1', 'MFCC_2', 'MFCC_3', 'MFCC_4', 'MFCC_5',
+           'MFCC_6', 'MFCC_7', 'MFCC_8', 'MFCC_9', 'MFCC_10', 'MFCC_11', 'MFCC_12', 'MFCC_13']
 
 def audio_selection(file_name, set_selection):
     try:
@@ -21,12 +23,22 @@ def audio_selection(file_name, set_selection):
         return np.full((13), np.nan)
     return my_data[:,1]
 
+#function below loads audio data and merges with attribute 'goodforairplane'
+#set_selection has to be 'dev' or 'test'
+
 def audio_data(df, set_selection):
     titles = df.index.tolist()
     split_data = pd.DataFrame(columns = indices).set_index('movie')
     for title in titles:
         split_data.loc[title] = audio_selection(df.loc[title]['filename'], set_selection)
     split_data = split_data.dropna()
+    
+    if set_selection == 'test':
+        return(pd.concat([split_data, data_test['goodforairplane']], axis=1, sort=True).reindex(split_data.index))
+    
+    if set_selection == 'dev':
+        return(pd.concat([split_data, data_dev['goodforairplane']], axis=1, sort=True).reindex(split_data.index))
+    
     return(split_data)
 
 # test data import
@@ -34,3 +46,13 @@ dev_data_audio = audio_data(data_dev, 'dev')
 
 # dev data import
 test_data_audio = audio_data(data_test, 'test')
+
+
+
+arff.dump('data/WEKA_files/audio_files/dev_data_audio.arff', dev_data_audio.values
+      , relation='audio_descriptors'
+      , names=dev_data_audio.columns)
+
+arff.dump('data/WEKA_files/audio_files/test_data_audio.arff', test_data_audio.values
+      , relation='audio_descriptors'
+      , names=test_data_audio.columns)
