3a9a3a7c0637e01d18630e187d1de35da08291a2
konstid <59444006+konstid@users.noreply.github.com>
2020-01-07 21:43:55 +0100

    Update text_table.py

--- text_table.py
+++ text_table.py
@@ -11,7 +11,6 @@ text_test = pd.read_csv('data/CoE_dataset/Test_Set/text_descriptors/tdf_idf_test
 
 new_test_keyword_selection = list(set(text_dev.columns).intersection(set(text_test.columns)))
 
-
 index_dev = list(text_dev.columns.values)
 text_dev = text_dev.dropna(axis = 1)
 
@@ -36,6 +35,10 @@ text_test = text_test.transpose()[new_test_keyword_selection]
 text_test.index = data_test.index.tolist()
 text_test[text_test != 0] = 1
 
+missing_test_keywords = list(set(temp) - set(new_test_keyword_selection))
+
+for keyword in missing_test_keywords:
+    text_test[keyword] = np.zeros((223, 1))
 
 text_dev = pd.concat([text_dev, data_dev['goodforairplane']], axis=1, sort=True).reindex(text_dev.index)
 text_test = pd.concat([text_test, data_test['goodforairplane']], axis=1, sort=True).reindex(text_test.index)
